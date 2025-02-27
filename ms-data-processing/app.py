886b8c4b4eb43ca79dfbc1b7234236a576141fba
Igli Balla <igliballa312@gmail.com>
2025-02-27 12:28:50 +0200

    Docker

--- ms-data-processing/app.py
+++ ms-data-processing/app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+import requests
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+
+from DataProcessingService import process
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/load_frame', methods=['POST'])
+def load_frame():
+    user_id = request.form.get('userId')
+    camera_id = request.form.get('camera_id')
+
+    file = request.files.get('file')
+    if file is None:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    image_bytes = file.read()
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    processed_frame = process(frame)
+    _, buffer = cv2.imencode('.jpg', processed_frame)
+
+    requests.post("http://localhost:5002/perform_recognition",
+                  json={'userId': user_id, 'camera_id': camera_id},
+                  files={'file': buffer.tobytes()}
+                  )
+    return
 
 
 if __name__ == '__main__':
--- ms-gateway/app.py
+++ ms-gateway/app.py
@@ -1,11 +1,61 @@
-from flask import Flask
+import requests
+from flask import Flask, request
+from KafkaConsumer import reading_stream
+import redis
+from flask_socketio import SocketIO, emit
+
+from UsersService import find_user
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*", message_queue="redis://redis:6379")
+redis_client = redis.Redis(host="redis", port=6379, db=0)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def initiate():
+    reading_stream()
+
+
+@socketio.on('connect')
+def handle_connection(message):
+    username = message.get('username')
+    user_id = find_user(username)
+
+    if user_id:  # perform proper authentication
+        redis_client.set(user_id, request.sid)
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    user_id = next((for k in redis_client.keys() if redis_client.get(k).decode() == request.sid), None)
+    if user_id:
+        redis_client.delete("user_id")
+        print(f"User {user_id} disconnected")
+
+
+@app.post('/notification')
+def notification(data):
+    user_id = data.get('user_id')
+    camera_id = data.get('camera_id')
+    notification_message = data.get('message')
+
+    session_id = redis_client.get(user_id)
+    if session_id:
+        socketio.emit("notification", {"camera": camera_id, "message": notification_message})
+        return 200
+    else:
+        return 404
+
+
+@app.get('/notifications')
+def archived_notifications(user_id, camera_id):
+    dictionary = {
+        "user_id": user_id,
+        "camera_id": camera_id,
+    }
+
+    data = requests.get("https://localhost:5005/get_notifications_of_user", params=dictionary)
+    return data.json()
 
 
 if __name__ == '__main__':
